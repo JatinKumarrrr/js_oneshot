b60a8930b661141ca41ce18fee371271ed8e3334
JatinKumarrrr <jatin27kumar2004@gmail.com>
2025-10-25 17:55:36 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,11 @@
+const accId  = 234234
+let accEmail = "jatin@gmail.com"
+var accPass =  "1234"
+accCity="dehradun"
+
+//accId = 2;
+accEmail = "hghgd@gmail.com";
+accPass= "4657" ;
+//console.log(accID);
+
+console.table([accId,accEmail,accPass])
