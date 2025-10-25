db16c839d613fea586d106e40869dc48353a6248
JatinKumarrrr <jatin27kumar2004@gmail.com>
2025-10-25 17:55:12 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+let name="jatin"
+
+let age = 18
+let isLoggedIn=false
+
+/*
+number = 2  to power 53
+bigint
+string=""
+boolean= true/false
+null = standalone value 
+undefined=
+slymbol  = unique
+*/
+console.log (typeof undefined)
